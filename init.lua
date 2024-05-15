96e6b41c9c81068716b9aa3965a4f999223ba278
Pedro Luz <pedromsluz@gmail.com>
2024-05-15 14:59:38 +0100

    added+config neotree

--- init.lua
+++ init.lua
@@ -23,17 +23,29 @@ local plugins = {
     'nvim-telescope/telescope.nvim', tag = '0.1.6',
     dependencies = { 'nvim-lua/plenary.nvim' }
   },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+  }
 }
 
 local opts = {}
  
 require("lazy").setup(plugins, opts)
 
+-- telescope
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 
+-- treesitter
 local config = require("nvim-treesitter.configs")
 config.setup({
   ensure_installed = {"lua", "go", "javascript", "rust", "vim", "c", "html"},
@@ -42,5 +54,11 @@ config.setup({
 })
 
 
+-- neotree
+vim.keymap.set('n', '<leader>n', ':Neotree filesystem left<CR>', {})
+
+
+
+-- theme catppuccin
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
